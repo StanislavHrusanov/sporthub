b4b8ce3f7d397b0c9ac76fafdba73d77dc49fad5
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-12-14 15:41:39 +0200

    Add env.js

---
+++ src/config/env.js
@@ -0,0 +1,5 @@
+exports.PORT = 3000;
+exports.CONNECTION_STRING = 'mongodb+srv://stanislavhrusanov:S9J1tTKyN6g8Q2w5@baroto87.o5rj3pt.mongodb.net/sporthubProject';
+exports.SECRET = 'ksjdh2635dh27dh72he.dsnj-dadanjdan';
+exports.SALT_ROUNDS = 10;
+exports.SESSION_NAME = 'user';
