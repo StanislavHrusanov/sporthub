9a193ada724cbdafd40a1230a20679df0c8f352e
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-12-14 15:49:31 +0200

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { initializeDatabase } = require('./config/database');
+const { PORT } = require('./config/env');
+const routes = require('./routes');
+
+const app = express();
+
+require('./config/handlebars')(app);
+
+app.use('/public', express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
+
+initializeDatabase()
+    .then(() => {
+        app.listen(PORT, () => console.log(`App is listening on port ${PORT}...`));
+    })
+    .catch((err) => {
+        console.log(`App cannot connect to database`, err);
+    });
