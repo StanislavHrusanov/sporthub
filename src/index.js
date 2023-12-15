551765943aea8c15fbbc4203d924776508daf624
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-12-15 16:35:45 +0200

    Add auth middleware to index.js and update routes.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { initializeDatabase } = require('./config/database');
 const { PORT } = require('./config/env');
+const cookieParser = require('cookie-parser');
+const { auth } = require('./middlewares/authMiddleware');
 const routes = require('./routes');
 
 const app = express();
@@ -9,7 +11,8 @@ require('./config/handlebars')(app);
 
 app.use('/public', express.static('public'));
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cookieParser());
+app.use(auth);
 app.use(routes);
 
 initializeDatabase()
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 const homeController = require('./controllers/homeController');
+const authController = require('./controllers/authController');
 
 routes.use('/', homeController);
+routes.use('/auth', authController);
 
 module.exports = routes;
