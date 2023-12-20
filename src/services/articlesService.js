bf444ad1a74b1e5df62f1f857353bb9f0d4c2148
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-12-20 19:28:57 +0200

    Add function getLatestArticles

--- src/services/articlesService.js
+++ src/services/articlesService.js
@@ -1,3 +1,5 @@
 const Article = require('../models/Article');
 
 exports.addArticle = (article) => Article.create(article);
+
+exports.getLatestarticles = () => Article.find().sort({ createdAt: -1 }).limit(13);
