0e6c233e748fcd2655faddcf4d183e0ac7efbc6b
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-12-20 14:24:53 +0200

    Register custom helper in handlebars to convert date on homePage

--- src/config/handlebars.js
+++ src/config/handlebars.js
@@ -1,5 +1,38 @@
 const hbs = require('express-handlebars');
 
+const hbars = hbs.create({});
+hbars.handlebars.registerHelper('homePageDate', function () {
+    const todayDate = new Date();
+    const days = {
+        Mon: 'Понеделник',
+        Tue: 'Вторник',
+        Wed: 'Сряда',
+        Thu: 'Четвъртък',
+        Fri: 'Петък',
+        Sat: 'Събота',
+        Sun: 'Неделя'
+    };
+
+    const months = {
+        Jan: 'Януари',
+        Feb: 'Февруари',
+        Mar: 'Март',
+        Apr: 'Април',
+        May: 'Май',
+        Jun: 'Юни',
+        Jul: 'Юли',
+        Aug: 'Август',
+        Sep: 'Септември',
+        Oct: 'Октомври',
+        Nov: 'Ноември',
+        Dec: 'Декември'
+    };
+
+    const [day, month, date, year] = todayDate.toDateString().split(' ');
+
+    return `${days[day]}, ${months[month]} ${date}, ${year}`;
+});
+
 module.exports = (app) => {
     app.engine('hbs', hbs.engine({
         extname: 'hbs'
