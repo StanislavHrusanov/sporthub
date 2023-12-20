acda86947c8cd05811fe0b1ad6d35218034d0874
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-12-20 19:30:57 +0200

    Add functionality to load latestArticles on Home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
+const articlesService = require('../services/articlesService');
 
 router.get('/', async (req, res) => {
+    try {
+        const latestArticles = await articlesService.getLatestArticles().lean();
+        const firstFour = latestArticles.slice(0, 4);
+        const fifthAndSixth = latestArticles.slice(4, 6);
+        const seventhAndEighth = latestArticles.slice(6, 8);
+        const nineth = latestArticles.slice(8, 9);
+        const tenthAndEleventh = latestArticles.slice(9, 11);
+        const twelfthAndThirteenth = latestArticles.slice(11, 13);
+        res.render('home/home', { firstFour, fifthAndSixth, seventhAndEighth, nineth, tenthAndEleventh, twelfthAndThirteenth });
+    } catch (error) {
+        res.send(error);
+    }
 
-    res.render('home/home');
 });
 
 module.exports = router;
