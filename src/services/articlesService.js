3c7ad67cf8cdb2693b15e283efce4a2c0d36a6c8
StanislavHrusanov <stanislavhrusanov@gmail.com>
2023-12-22 13:31:28 +0200

    Add function to get article details

--- src/services/articlesService.js
+++ src/services/articlesService.js
@@ -7,3 +7,5 @@ exports.getLatestArticles = () => Article.find().sort({ createdAt: -1 }).limit(1
 exports.getArticlesOfExactSport = (sport) => Article.find({ sport: sport }).sort({ createdAt: -1 }).populate('author');
 
 exports.getArticlesOfExactLeague = (sport, league) => Article.find({ sport: sport, league: league }).sort({ createdAt: -1 }).populate('author');
+
+exports.getOne = (articleId) => Article.findById(articleId).populate('author');
