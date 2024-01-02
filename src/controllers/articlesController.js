1d8a5cf6751ef48eafab2a1a6cb72bd7d2820f52
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-01-02 13:27:11 +0200

    Add functionality to change pages on league France

--- src/controllers/articlesController.js
+++ src/controllers/articlesController.js
@@ -140,9 +140,16 @@ router.get('/worldFootball/Germany', async (req, res) => {
 });
 
 router.get('/worldFootball/France', async (req, res) => {
+    let page = req.query.page ? Number(req.query.page) : 1;
+    let limit = 12;
+
     try {
-        const articles = await articlesService.getArticlesOfExactLeague('ФУТБОЛ СВЯТ', 'Франция').lean();
-        res.render('articles/category', { articles });
+        const articles = await articlesService.getArticlesOfExactLeague('ФУТБОЛ СВЯТ', 'Франция', page, limit).lean();
+        const count = await articlesService.getArticlesCountOfExactLeague('ФУТБОЛ СВЯТ', 'Франция');
+        const pages = Math.ceil(count / limit);
+        let isLast = page >= pages;
+        let isFirst = page == 1;
+        res.render('articles/category', { articles, pages, req, isFirst, isLast });
 
     } catch (error) {
         res.send(error);
