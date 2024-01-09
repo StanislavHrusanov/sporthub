0a2d86a19086a66062c1b4ed3cac1eeef2d345f9
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-01-09 14:01:15 +0200

    Add isAdmin to User model

--- src/models/User.js
+++ src/models/User.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
 });
 
