5e2916a4e1f2497514c633cd474e0d3142f61814
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-03-13 11:42:58 +0200

    Add sports.js

---
+++ src/utils/sports.js
@@ -0,0 +1,9 @@
+exports.sports = {
+    basketball: 'БАСКЕТБОЛ',
+    volleyball: 'ВОЛЕЙБОЛ',
+    tennis: 'ТЕНИС',
+    athletics: 'ЛЕКА АТЛЕТИКА',
+    motorSports: 'МОТОРНИ СПОРТОВЕ',
+    combatSports: 'БОЙНИ СПОРТОВЕ',
+    others: 'ДРУГИ'
+}
