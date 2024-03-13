1bddea4b30487b67cd955e2b86e5d4198909f20a
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-03-13 11:30:41 +0200

    Add leagues.js

---
+++ src/utils/leagues.js
@@ -0,0 +1,12 @@
+exports.leagues = {
+    England: 'Англия',
+    Spain: 'Испания',
+    Italy: 'Италия',
+    Germany: 'Германия',
+    France: 'Франция',
+    ChampionsLeague: 'ШЛ',
+    EuropaLeague: 'Лига Европа',
+    EuropaConferenceLeague: 'Лига на конференциите',
+    NationalTeams: 'Национални отбори',
+    OtherLeagues: 'Други'
+}
